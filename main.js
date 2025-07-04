cc247dedff6d451182611b5295b5c0cf9af45cc5
Yestay <yestayyessenbay@gmail.com>
2025-07-04 21:36:19 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: 'Socks',
+            description: 'A pair of warm, cozy socks to keep your feet comfortable.',
+        }
+    }
+})
