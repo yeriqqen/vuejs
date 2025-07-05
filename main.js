3c4ac8f3572ba13462d65d9f6ba92943dd167009
Yestay <yestayyessenbay@gmail.com>
2025-07-05 12:55:55 +0900

    feat: computed properties

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 const app = Vue.createApp({
     data: function() {
         return {
+            brand: 'Vue Mastery',
             product: 'Premium Cotton Socks',
             description: 'Experience ultimate comfort with our premium cotton socks. Perfect for daily wear, these socks provide exceptional softness and durability.',
             image: './assets/images/socks_green.jpg',
+            imageOutOfStock: './assets/images/out_of_stock.png',
             url: 'https://www.vuemastery.com/courses/intro-to-vue-3/intro-to-vue/',
-            onSale: true,
             sizes: ['S', 'M', 'L', 'XL'],
             selectedSize: null,
             cartItems: 0,
             variants: [
                 {
-                    id: 2234,  image: './assets/images/socks_green.jpg', quantity: 10, color: 'green'
+                    id: 2234,  image: './assets/images/socks_green.jpg', quantity: 10, color: 'green', quantity: 5
                 },
                 {
-                    id: 2235, image: './assets/images/socks_blue.jpg', quantity: 0, color: 'blue'
+                    id: 2235, image: './assets/images/socks_blue.jpg', quantity: 0, color: 'blue', quantity: 0
                 }
             ],  
-            inStock: false,
+            selectedVariant: 0,
         }
     },
     methods: {
@@ -32,6 +33,24 @@ const app = Vue.createApp({
         },
         updateImage(image) {
             this.image = image;
+            this.selectedVariant = this.variants.findIndex(variant => variant.image === image);
+        },
+        nextColorImage(image) {
+            this.image = this.variants.findIndex(variant => variant.image === image) == 0 ? this.variants[1].image : this.variants[0].image;
+        },
+    },
+    computed: {
+        inStock() {
+            return this.variants[this.selectedVariant].quantity > 0;
+        },
+        onSale() {
+            return this.variants[this.selectedVariant].quantity > 0 && this.variants[this.selectedVariant].quantity <= 5;
+        },
+        title() {
+            if( this.onSale ) {
+                return this.brand + ' ' + this.product + ' are on sale!';
+            }
+            return this.brand + ' ' + this.product;
         }
     }
 })
